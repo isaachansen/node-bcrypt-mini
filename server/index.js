ceacf6616bf874d02423ff40ecd5ea8bd4e53735
Isaac Hansen <isaac@Isaacs-MacBook-Pro-2.local>
2019-10-24 12:41:35 -0700

    mini

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
-require('dotenv').config();
-const express = require('express');
-const session = require('express-session');
-const bcrypt = require('bcryptjs');
-const massive = require('massive');
+require("dotenv").config();
+const express = require("express");
+const session = require("express-session");
+const bcrypt = require("bcryptjs");
+const massive = require("massive");
 
 const app = express();
 
@@ -19,7 +19,51 @@ app.use(
 );
 
 massive(CONNECTION_STRING).then(db => {
-  app.set('db', db);
+  app.set("db", db);
+});
+
+app.post("/auth/signup", (req, res, next) => {
+  const { email, password } = req.body;
+  const db = req.app.get("db");
+  db.check_user_exists(email).then(user => {
+    if (user.length) {
+      res.status(403).send("Nice try Sean!");
+    } else {
+      const saltRounds = 12;
+      bcrypt.genSalt(saltRounds).then(generatedSalt => {
+        bcrypt.hash(password, generatedSalt).then(hashedPassword => {
+          db.create_user([email, hashedPassword]).then(([createdUser]) => {
+            req.session.user = {
+              id: createdUser.id,
+              email: createdUser.email
+            };
+            res.status(200).send(req.session.user);
+          });
+        });
+      });
+    }
+  });
+});
+
+app.post("/auth/login", async (req, res, next) => {
+  const db = req.app.get("db");
+  const { email, password } = req.body;
+
+  const foundUser = await db.check_user_exists(email);
+  if (!foundUser) {
+    res.status(400).send("Nice try again Sean, you ain't getting in my app");
+  } else {
+    const authenticated = await bcrypt.compare(password, foundUser[0].user_password);
+    if (authenticated) {
+      req.session.user = {
+        id: foundUser[0].id,
+        email: foundUser[0].email
+      };
+      res.status(200).send(req.session.user);
+    } else {
+      res.status(403).send("I SAID NO SEAN!!!!!");
+    }
+  }
 });
 
 app.listen(SERVER_PORT, () => {
--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: '',
-      password: '',
+      email: "",
+      password: "",
       loggedInUser: {}
     };
   }
 
-  async login() {}
+  async login() {
+    const { email, password } = this.state;
+    const user = await axios.post("/auth/login", { email, password });
+    this.setState({
+      loggedInUser: user.data,
+      email: "",
+      password: ""
+    });
+  }
 
-  async signup() {}
+  signup() {
+    const { email, password } = this.state;
+    axios.post("/auth/signup", { email, password }).then(res => {
+      this.setState({
+        loggedInUser: res.data,
+        email: "",
+        password: ""
+      });
+    });
+  }
 
   logout() {}
 
@@ -50,9 +67,9 @@ class App extends Component {
 
         <hr />
 
-        <h4>Status: {loggedInUser.email ? 'Logged In' : 'Logged Out'}</h4>
+        <h4>Status: {loggedInUser.email ? "Logged In" : "Logged Out"}</h4>
         <h4>User Data:</h4>
-        <p> {loggedInUser.email ? JSON.stringify(loggedInUser) : 'No User'} </p>
+        <p> {loggedInUser.email ? JSON.stringify(loggedInUser) : "No User"} </p>
         <br />
       </div>
     );
